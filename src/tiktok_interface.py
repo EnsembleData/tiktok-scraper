565f0a4f5d13d0f080eb292b425926c2273c5448
andrea <andrea_ramazzina@hotmail.it>
2021-10-15 20:33:19 +0200

    Add description API functions

--- src/tiktok_interface.py
+++ src/tiktok_interface.py
@@ -12,7 +12,8 @@ class Tiktok_I_IH(Interface_IH):
 			depth (int, optional): [description]. Defaults to 1.
 
 		Returns:
-			[type]: [description]
+			dictionary: content of the response
+			bool: True if the request succeeded 
 		"""
 
 		end_point = self.req_url+ '/tt/hashtag/posts'
@@ -28,7 +29,8 @@ class Tiktok_I_IH(Interface_IH):
 			name (string): username (a.k.a. handle)
 
 		Returns:
-			[type]: [description]
+			dictionary: content of the response
+			bool: True if the request succeeded 
 		"""        
 		end_point = self.req_url+ '/tt/user/info'
 		payload = {'username':name, 'token':self.token_IH_API}
@@ -37,14 +39,32 @@ class Tiktok_I_IH(Interface_IH):
 		return r
 
 	def get_user_posts(self, username, depth = 1):	
+		"""Get the latest posts of a user given the username
+
+		Args:
+			username (string): username (a.k.a. handle)
+
+		Returns:
+			dictionary: content of the response
+			bool: True if the request succeeded 
+		"""                
 		end_point = self.req_url+ '/tt/user/posts'
 		payload = {'username':username, 'depth': depth, 'token':self.token_IH_API}
 
 		r = self.send_request(end_point, payload)
 		return r
 
-	def get_user_posts_id(self, secUid, depth = 1):	
-		end_point = self.req_url + "/tt/user/posts-from-id"
+	def get_user_posts_secuid(self, secUid, depth = 1):	
+		"""Get the latest posts of a user given the Secuid
+
+		Args:
+			username (string): username (a.k.a. handle)
+
+		Returns:
+			dictionary: content of the response
+			bool: True if the request succeeded 
+		"""   
+		end_point = self.req_url + "/tt/user/posts-from-secuid"
 		payload = {'secUid':secUid, 'depth': depth, 'token':self.token_IH_API}
 
 		r = self.send_request(end_point, payload)
@@ -59,7 +79,7 @@ class Tiktok_I_IH(Interface_IH):
 			days (int, optional): number of last days to retrieve. Defaults to 10.
 
 		Returns:
-			dictionary: content of the response, None if it failed
+			dictionary: content of the response
 			bool: True if the request succeeded 
 		"""
 
@@ -70,6 +90,18 @@ class Tiktok_I_IH(Interface_IH):
 		return r     
 
 	def get_post_from_keyword(self, keyword, period = 0, sorting = 0, cursor = 0):	
+		"""Get the results of a keybard search (posts, etc.)
+
+		Args:
+			keyword (string): keyword (hashtag, name, etc.) to search
+			period (string): how many days. Options: "0" "1" "7" "30" "90" "180"
+			sorting (bool): sorting order. Either 0 or 1
+			cursor (int): At the beginning set to 0. Then it is given by the previous call.
+
+		Returns:
+			dictionary: content of the response
+			bool: True if the request succeeded 
+		"""		
 		end_point = self.req_url+ '/tt/keyword/search'
 		payload = {'name':keyword, 'period':period, "sorting":sorting, "cursor":cursor, 'token':self.token_IH_API}
 
@@ -77,6 +109,16 @@ class Tiktok_I_IH(Interface_IH):
 		return r  
 
 	def get_comments(self, aweme_id, cursor = 0):	
+		"""Fetch latest comments of a post
+
+		Args:
+			aweme_id (string): aweme_id of a post 
+			cursor (int): At the beginning set to 0. Then it is given by the previous call.
+
+		Returns:
+			dictionary: content of the response
+			bool: True if the request succeeded 
+		"""				
 		end_point = self.req_url+ '/tt/post/comments'
 		payload = {"aweme_id":aweme_id, "cursor":cursor, 'token':self.token_IH_API}
 
@@ -84,7 +126,18 @@ class Tiktok_I_IH(Interface_IH):
 		return r  
 
 	def get_comment_replies(self, aweme_id, comment_id, cursor = 0):	
-		end_point = self.req_url+ '/tt/post/comments'
+		"""Fetch latest replies of a comment (of a post)
+
+		Args:
+			aweme_id (string): aweme_id of a post 
+			comment_id (string): comment_id of a comment of that post
+			cursor (int): At the beginning set to 0. Then it is given by the previous call.
+
+		Returns:
+			dictionary: content of the response
+			bool: True if the request succeeded 
+		"""			
+		end_point = self.req_url+ '/tt/post/comments-replies'
 		payload = {"aweme_id":aweme_id, "comment_id":comment_id, "cursor":cursor, 'token':self.token_IH_API}
 
 		r = self.send_request(end_point, payload)
