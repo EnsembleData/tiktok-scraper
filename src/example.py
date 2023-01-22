fbded5f6509b5f48efd260b717af964bf3033d53
andrea <andrea_ramazzina@hotmail.it>
2023-01-22 10:57:52 +0100

    update library

--- src/example.py
+++ src/example.py
@@ -2,17 +2,17 @@ from tiktok_interface import Tiktok_I_IH
 
 
 # Get a free token at www.influencerhunters.com
-TOKEN = "XXXXXXXXXXXXX"
+TOKEN = "INSERT YOUR TOKEN HERE"
 
 #Initialize sender class 
 tt = Tiktok_I_IH(token_IH_API=TOKEN)
 
 #Send the request to the IH server
 print("sending the request..")
-res, success = tt.get_user_posts(username = "charlidamelio", depth = 2)
+res, success = tt.get_hashtag_posts(name = "magic", cursor = 0)
 
 if success:
     print("Success!")
-    print("Retrieved the last",len(res),"posts")
+    print("Retrieved the last",len(res["data"]),"posts")
 else:
     print("Something went wrong, check the response for more information. \n(Did you insert a valid token?)")
